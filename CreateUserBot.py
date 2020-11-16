f2dccb54de1c7cf4b845e4db4511a634bc6a6233
CharlietheVIII8 <61073757+CharlietheVIII8@users.noreply.github.com>
2020-11-15 20:08:46 -0500

    enter_first_name created

--- CreateUserBot.py
+++ CreateUserBot.py
@@ -1,9 +1,11 @@
+import names
 import time
 
 from selenium import webdriver
+from selenium.common.exceptions import ElementClickInterceptedException
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.keys import Keys
-from selenium.common.exceptions import ElementClickInterceptedException
+
 
 class CreateUserBot:
     DRIVER_PATH = 'chromedriver.exe'
@@ -15,6 +17,8 @@ class CreateUserBot:
         options.add_argument("start-maximized")
         self.driver = webdriver.Chrome(options=options, executable_path=self.DRIVER_PATH)
         self.email, self.password = email, password
+        self.first_name = names.get_first_name(gender='male')
+        self.last_name = names.get_last_name()
 
     def initiate_join(self):
         self.driver.get(self.BASE_URL)
@@ -38,6 +42,15 @@ class CreateUserBot:
         self.driver.find_element_by_class_name('signup2017-button').click()
         time.sleep(5)
 
+    def initiate_profile_creation(self):
+        self.driver.find_element_by_class_name('onboarding-stepIntro-button').click()
+        time.sleep(5)
+
+    def enter_first_name(self):
+        self.driver.find_element_by_name('name').send_keys(self.first_name, Keys.RETURN)
+        time.sleep(5)
+        self.driver.find_element_by_class_name('profileDetails-button--next profileDetails-button--fixed').click()
+        time.sleep(5)
 
 
 if __name__ == '__main__':
@@ -45,3 +58,4 @@ if __name__ == '__main__':
     bot.initiate_join()
     bot.enter_email()
     bot.enter_password()
+    bot.initiate_profile_creation()
