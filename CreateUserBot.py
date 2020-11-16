d5d8de181ad1b2bc4b2fc04ffc2277b825d75180
CharlietheVIII8 <61073757+CharlietheVIII8@users.noreply.github.com>
2020-11-15 19:33:18 -0500

    initiate_join created

--- CreateUserBot.py
+++ CreateUserBot.py
@@ -1,3 +1,22 @@
-class CreateUserBot:
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
 
-    def __init__(self):
+
+class CreateUserBot:
+    DRIVER_PATH = 'chromedriver.exe'
+    BASE_URL = "https://www.okcupid.com/"
+
+    def __init__(self, headless=False):
+        options = Options()
+        options.headless = headless
+        options.add_argument("start-maximized")
+        self.driver = webdriver.Chrome(options=options, executable_path=self.DRIVER_PATH)
+
+    def initiate_join(self):
+        self.driver.get(self.BASE_URL)
+        self.driver.find_element_by_xpath('//*[@id="main_content"]/div/div/div[5]/a').click()
+
+
+if __name__ == '__main__':
+    bot = CreateUserBot()
+    bot.initiate_join()
--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # OKCupid
 OKCupid Scraper and Analyzer
+
+Chrome Version:
+
+The chromedriver in the repository is for Chrome 86. If you have a different version, you will need to replace it with the proper driver.
